23586ed933ecddc65aa0609d5675631c92a0a258
sanchit <10dsanchitbis.virdi@gmail.com>
2026-09-15 18:56:58 +0530

    render address added

--- random-password generator/frontend/src/lib/axios.js
+++ random-password generator/frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosinstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5003/api" : "/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5003/api" : "https://ranpass.onrender.com/api",
     withCredentials:true,
 })
 
