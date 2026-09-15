34d7fa2d9d26f40034628dc4fba8e35ad3e8401d
sanchit <10dsanchitbis.virdi@gmail.com>
2026-09-15 19:32:30 +0530

    problem regarding cookie fixed..

--- random-password generator/backend/src/lib/uteis.js
+++ random-password generator/backend/src/lib/uteis.js
@@ -1,13 +1,20 @@
 import jwt from "jsonwebtoken"
 
-export  const generateToken=(userId,res)=>{
-    const token=jwt.sign({userId},process.env.JWT_SECRET,{
-        expiresIn:"7d"
-    })
-    res.cookie("jwt",token,{
-        maxAge:7*24*60*60*1000,
-        httpOnly:true,
-    })
-     
+export const generateToken = (userId, res) => {
+    const token = jwt.sign(
+        { userId },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "7d"
+        }
+    );
+
+    res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        secure: true,
+        sameSite: "none"
+    });
+
     return token;
-}
+};
